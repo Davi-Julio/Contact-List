bc2201f05477a0bbc0deab27b37bec8a17cdbeb9
Davi Júlio <davijulio550@gmail.com>
2023-12-17 13:49:49 -0300

    Alteração Db_Name

--- Model/config.php
+++ Model/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$localhost ='localhost';
-$user = 'root';
-$pass ='';
-$db_name = "users";
+$localhost ='sql203.infinityfree.com';
+$user = 'if0_35635150';
+$pass ='ozxLNiRgFcReh';
+$db_name = "if0_35635150_contacts";
 
 $mysqli = new mysqli($localhost,$user,$pass,$db_name);
 
